84a4648c8700966761908a04e5d19268cf61a183
thaisdevfront <thais.devfrontend@gmail.com>
2023-04-10 18:00:22 -0300

    ..

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,50 +25,39 @@ app.innerHTML=`
         
     
     </div>
-    <div id="jobs">
+
+    <div id="jobs"> 
         <div class="container">
             <div class="content-text">
                 <span class="element">DIGITAL HARMONY</span>
-                <h3>CRIE, DESENVOLVA E CONQUISTE A COMUNICAÇÃO IDEAL PARA NOVOS CLIENTES.</h3>
+                <h3>ENCONTRE PRODUTOS IDEAIS PARA UMA COMUNICAÇÃO EFICIENTE.</h3>
             </div>
+            <button class="btn primary xs-hidden">
+                <a href="https://api.whatsapp.com/send?phone=5512981021517&text=%" target="_black">Falar com especialista<i class="fa-regular fa-comments"></i></a>
+            </button> 
         </div>
 
-            <div class="">
+        <div class=" " style="
+        width: 100%;
+    ">
 
             <div class="swiper mySwiper">
                 <div class="swiper-wrapper">
                     <div class="swiper-slide"> 
                             <img class="sm-hidden" src="assets/img/app-mockup.png">
-                            <img class="xs-hidden" src="assets/img/app-sm.png">
-                             
-                       
-                    </div>
-                    <div class="swiper-slide">
-                        <img class="sm-hidden" src="assets/img/app-mockup.png">
-                        <img class="xs-hidden" src="assets/img/app-sm.png">
-
-
-                    </div>
-                    <div class="swiper-slide">
-                        <img class="sm-hidden" src="assets/img/app-mockup.png">
-                        <img class="xs-hidden" src="assets/img/app-sm.png">
-                        
-                        
-
-                    </div>
+                            <img class="xs-hidden" src="assets/img/app-sm.png"> 
+                    </div> 
+                    <div class="swiper-slide"> 
+                            <img class="sm-hidden" src="assets/img/app-mockup.png">
+                            <img class="xs-hidden" src="assets/img/app-sm.png"> 
+                    </div> 
                 </div>
-                
-            </div>
-
-        </div>
-
-
-        
-
-        <div>
-        <button class='btn primary'>Falar com especialista </button>
-
-        </div>
+            </div> 
+             
+            <button class="btn primary sm-hidden">
+            <a href="https://api.whatsapp.com/send?phone=5512981021517&text=%" target="_black">Falar com especialista<i class="fa-regular fa-comments"></i></a>
+        </button> 
+        </div> 
 
     </div>
 
